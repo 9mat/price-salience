f7374737d6818962e7cad04056091055dad2fc41
Duong Hai Long <dhlong.hp@gmail.com>
2016-10-15 15:23:07 +0800

    modify nloglf to handle nan due to log

--- nloglf.m
+++ nloglf.m
@@ -1,11 +1,11 @@
 function f = nloglf(theta, Data, n)
 likelihood = choiceProb(theta, Data, n);
+loglf = log(likelihood);
 
-if any(isnan(likelihood))
+if any(isnan(loglf))
     f = 1e20*(1+norm(theta)^2);
     return;
 end
 
-loglf = log(likelihood);
 f = -sum(loglf);
 end
